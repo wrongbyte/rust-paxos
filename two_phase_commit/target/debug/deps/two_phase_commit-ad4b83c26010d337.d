2ea4375068abff7b3fabab551bd4824e26088089
by <57643375+wrongbyte@users.noreply.github.com>
2024-12-02 21:49:17 -0300

    init

--- two_phase_commit/target/debug/deps/two_phase_commit-ad4b83c26010d337.d
+++
@@ -1,5 +0,0 @@
-/home/wrongbyte/projects/silly-paxos-rust/two_phase_commit/target/debug/deps/libtwo_phase_commit-ad4b83c26010d337.rmeta: src/main.rs
-
-/home/wrongbyte/projects/silly-paxos-rust/two_phase_commit/target/debug/deps/two_phase_commit-ad4b83c26010d337.d: src/main.rs
-
-src/main.rs:
