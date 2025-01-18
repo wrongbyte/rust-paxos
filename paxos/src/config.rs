fe67400f0dd69827a6f9f5d6b11cfa135331fa78
by <57643375+wrongbyte@users.noreply.github.com>
2025-01-17 21:42:01 -0300

    update readme

--- paxos/src/config.rs
+++ paxos/src/config.rs
@@ -4,11 +4,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// Number of nodes
+    /// Number of nodes in the simulation.
     #[arg(short, long, default_value_t = 3)]
     pub nodes: usize,
 
-    /// Number of rounds
+    /// Number of rounds.
     #[arg(short, long, default_value_t = 10)]
     pub rounds: usize,
 }
