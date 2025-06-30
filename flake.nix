3a48350909f577e333e17fc98378bc1657180d8d
by <57643375+wrongbyte@users.noreply.github.com>
2025-06-29 21:26:10 -0300

    chore: add run command

--- flake.nix
+++ flake.nix
@@ -19,16 +19,23 @@
         overlays = builtins.attrValues overlays; 
       };
       rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      run = pkgs.writeShellScriptBin "run" ''
+        cargo run --manifest-path paxos/Cargo.toml -- "$@"
+      '';
     in 
     with pkgs; {
       devShells.default = pkgs.mkShell {
         buildInputs = [
           rustToolchain
           pkgs.sqlite
+          run
         ];
 
         shellHook = ''
-          printf "\nEnvironment is set up! ٩(◕‿◕｡)۶\n\n"
+          printf "\nEnvironment is set up! ٩(◕‿◕｡)۶\n
+          To run the project, use \`run\`.\n\n"
+        }
         '';
       };
     }
